02797cdc6fc42a884e49d385d2fb510770e3d2df
thejsj <jorge.silva@thejsj.com>
2015-03-19 23:02:36 -0700

    Update package.json

---
+++ config/default.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+ * Configuration Structure
+ *
+ * default.js
+ * - test.js
+ * - development.js
+ * - - staging.js
+ * - - - production.js
+ */
+var config = {
+  'rethinkdb': {
+    'host': 'localhost',
+    'port': 28015,
+    'db': 'realtime_photo_sharing'
+  },
+  'ports': {
+    'http': 8000
+  },
+  'url': 'localhost',
+};
+module.exports = config;
