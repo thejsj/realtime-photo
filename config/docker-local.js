19a62abf6391739fac02b668e72500621ff4ebbe
thejsj <jorge.silva@thejsj.com>
2015-03-24 11:35:23 -0700

    Adding docker local config

---
+++ config/docker-local.js
@@ -0,0 +1,17 @@
+/*jshint node:true */
+'use strict';
+/**
+ * Inherits from `default.js`
+ */
+var config = {
+  'rethinkdb': {
+    'host': process.env.RETHINKDB_PORT_28015_TCP_ADDR,
+    'port': process.env.RETHINKDB_PORT_28015_TCP_PORT
+  },
+  'ports' : {
+    'http' : process.env.PORT,
+  },
+  'url': 'docker.dev'
+};
+
+module.exports = config;
